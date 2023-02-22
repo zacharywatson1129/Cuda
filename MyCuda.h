0e44d165e1e77364b320c0e4b39fc2aa34e4a5b1
Zachary Watson <40725419+zacharywatson1129@users.noreply.github.com>
2023-02-22 14:44:00 -0600

    Add files via upload

--- MyCuda.h
+++ MyCuda.h
@@ -1,3 +1,4 @@
+/// Defined in MyCuda.h: Gets the last CUDA error message and prints it out, along with file and line #.
 void myCudaErrorCheck(const char *file, int line)
 {
 	cudaError_t error;
@@ -5,7 +6,13 @@ void myCudaErrorCheck(const char *file, int line)
 
 	if(error != cudaSuccess)
 	{
-		printf("\n CUDA message = %s, File = %s, Line = %d\n", cudaGetErrorString(error), file, line);
+		printf("\n CUDA message = %s, File = %s, Line = %d\n", cudaGetErrorString(error), file, line-1);
 		exit(0);
 	}
+}
+
+/// Defined in MyCuda.h: Returns the number of blocks needed given the number of threads per block and an N value. 
+int getNumberBlocks(int numberOfThreads, int n)
+{
+	return (n - 1)/numberOfThreads + 1;
 }
